73c117bddb7fcad9be7aaf94880312cf40b150b9
waffle690 <119129001+waffle690@users.noreply.github.com>
2023-01-28 12:42:26 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+
+2
+// function App() {
+//   const [value, setValue] = useState('');
+//   const [value2, setValue2] = useState('');
+//   const [value3, setValue3] = useState('');
+//   const [value4, setValue4] = useState('');
+//   const [value5, setValue5] = useState('');
+//   return <div>
+//   <input value={value} onChange={event => setValue(+event.target.value)} />
+//   <input value={value2} onChange={event => setValue2(+event.target.value)} />
+//   <input value={value3} onChange={event => setValue3(+event.target.value)} />
+//   <input value={value4} onChange={event => setValue4(+event.target.value)} />
+//   <input value={value5} onChange={event => setValue5(+event.target.value)} />
+//   <p>text: {(value+value2+value3+value4+value5)/5}</p> 
+//   </div>;
+// }  
+
+
+1
+// function App() {
+//   const [value, setValue] = useState('');
+//   return <div>
+//   <input value={value} onChange={event => setValue(+event.target.value)} />
+//   <p>text: {(value-32)/1.8}</p> 
+//   </div>;
+// }
+
+export default App
